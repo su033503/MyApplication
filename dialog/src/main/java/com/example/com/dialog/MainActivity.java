aa8f39106f95cd602eb99d18ebf27387203b5e1f
su033503 <su033503@naver.com>
2015-11-05 11:14:50 +0900

    dialog

---
+++ dialog/src/main/java/com/example/com/dialog/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.com.dialog;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvName, tvEmail;
+    Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText;
+    View dialogView, toastView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("사용자 정보 입력");
+
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dialogView = (View) View.inflate(MainActivity.this, R.layout.dialog1, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setIcon(R.drawable.ic_menu_cc_am);
+                dlg.setView(dialogView);
+                dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+                dlgEdtName.setText(tvName.getText().toString());
+                dlgEdtEmail.setText(tvEmail.getText().toString());
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        tvName.setText(dlgEdtName.getText().toString());
+                        tvEmail.setText(dlgEdtEmail.getText().toString());
+                    }
+                });
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast toast = new Toast(MainActivity.this);
+                        toastView = (View) View.inflate(MainActivity.this, R.layout.toast1, null);
+                        toastText = (TextView) toastView.findViewById(R.id.toastText1);
+                        toastText.setText("취소했습니다");
+                        Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+                        Point outSize = new Point();
+                        display.getSize(outSize);
+                        int xOffset = (int) (Math.random() * outSize.x);
+                        int yOffset = (int) (Math.random() * outSize.y);
+                        toast.setGravity(Gravity.TOP | Gravity.LEFT, xOffset, yOffset);
+                        toast.setView(toastView);
+                        toast.show();
+                    }
+                });
+                dlg.show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
